3a4af41e1e74ac3670824f7912bee165157b984f
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-11-11 18:29:08 +0600

    used dotenv on the server side

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 1000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@tawsifahmed.18d0gsh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+// middle wares
 app.use(cors());
 app.use(express.json());
 
